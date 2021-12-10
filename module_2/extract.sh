815273afc764b1705f4f9df4ace89576428d9fa2
T. H. Arjun <arjun.thekoot@research.iiit.ac.in>
2021-12-10 06:24:50 +0530

    Added extraction script

---
+++ module_2/extract.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -A research
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=24
+#SBATCH --time=4-00:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --exclude=gnode[01-45]
+#SBATCH --mail-type=END,FAIL
+#SBATCH -o /scratch/arjun_drqa_out.txt
+#SBATCH --job-name=drqa_out
+module load cuda/10.1
+module load cudnn/7.6-cuda-10.0
+module load TensorRT/7.2.2.3
+source /home2/arjunth2001/miniconda3/etc/profile.d/conda.sh
+conda activate py37
+mkdir -p /scratch/arjunth2001/
+scp -r arjunth2001@ada.iiit.ac.in:'/share1/arjunth2001/drqa_2/data' /scratch/arjunth2001
+scp -r arjunth2001@ada.iiit.ac.in:'/share1/arjunth2001/train_df.csv' /scratch/arjunth2001
+scp -r arjunth2001@ada.iiit.ac.in:'/share1/arjunth2001/dev_df.csv' /scratch/arjunth2001
+papermill  --request-save-on-cell-execute --log-output --log-level INFO --progress-bar extract.ipynb /scratch/arjunth2001/extract.ipynb
+rclone copy -P  /scratch/arjunth2001/output1.csv onedrive:/drqa_out/
+rclone copy -P  /scratch/arjunth2001/output2.csv onedrive:/drqa_out/
